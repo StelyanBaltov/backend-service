5039ac19fbb89d0a8fe56c5b5abc512bf96e099a
Stelyan Baltov <stelyan.baltov@gmail.com>
2018-08-31 14:14:01 +0300

    .

--- db_connect.js
+++ db_connect.js
@@ -3,18 +3,11 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "stelko",
+    password: "123",
     database: "baltov"
 });
 
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
-
-    var sql = "CREATE TABLE users (firstName VARCHAR(255), lastName VARCHAR(255))";
-
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Table created");
-    });
 });

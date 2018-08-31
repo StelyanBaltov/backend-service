ef850add53a78c8f7d5e9b2328870c02838443d2
Stelyan Baltov <stelyan.baltov@gmail.com>
2018-08-31 14:44:51 +0300

    make db connection a module and add users api

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const db = require('./db_connect')
 
 const port = process.env.PORT || 2003
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    console.log(res.send('Hello World'))
+})
+
+app.get('/users', (req, res) => {
+    const sql = "SELECT * FROM users";
+    db.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        const users = result.map((user) => {
+            return user;
+        })
+        res.send(users);
+    });
+
 })
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
